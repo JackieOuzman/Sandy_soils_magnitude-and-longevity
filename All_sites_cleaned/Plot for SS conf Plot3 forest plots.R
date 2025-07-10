4bafa5b71d2954b46c6105a7aaa76c20515c425e
JackieOuzman <jackie.ouzman@csiro.au>
2025-07-10 15:01:54 +0930

    using new cleaned data set

---
+++ All_sites_cleaned/Plot for SS conf Plot3 forest plots.R
@@ -0,0 +1,276 @@
+library(ggplot2)
+library(readxl)
+library(tidyverse)
+library(stringr)
+
+library(dmetar)
+library(tidyverse)
+library(meta)
+
+library(forcats)
+
+## plot and analsyis for preso
+
+
+
+
+df <- read.csv("N:/sandy soils conference/data/All_sites_cleaned/control_metadata_contraints_tillage_only_cleaned.csv" )
+
+
+# keep only one yield output
+str(df)
+names(df)
+df_modified <- df %>% 
+  dplyr::select(tillage_amendments_class,
+         tillage_class,
+         site_display,
+         year,
+         relative_yld_change,
+         yield_gain,
+         yield,
+         control_yield,
+         tillage_amendments_class
+  ) %>% 
+  dplyr::mutate(site_year = paste0(site_display,"_", year)) %>% 
+  filter(tillage_amendments_class != "Unmodified_amendment")
+
+str(df_modified)
+
+
+
+
+### Option 2 means ----
+#### using Relative yield mean of the treatments ---
+
+str(df_modified)
+df_modified_summary <- df_modified %>% dplyr::group_by(site_display) %>% 
+  dplyr::summarise(mean = mean( relative_yld_change, na.rm = TRUE),
+            sd = sd( relative_yld_change, na.rm = TRUE),
+            n = n())
+                                        
+df_modified_summary
+m.mean <- metamean(n = n,
+                   mean = mean,
+                   sd = sd,
+                   studlab = site_display,
+                   data = df_modified_summary,
+                   sm = "MRAW",
+                   fixed = FALSE,
+                   random = TRUE,
+                   method.tau = "REML",
+                   method.random.ci = "HK",
+                   title = "Option 2")
+summary(m.mean)
+
+
+
+### I can plot this a more manual way but I need to export some data from the analysis
+
+dim(df_modified_summary)
+#how many rows of data ?
+forest_plot_input <- data.frame(
+  #Index = seq(1:26), ## This provides an order to the data
+  label = m.mean$studlab,
+  SMD = m.mean$TE,
+  LL = m.mean$lower,
+  UL =  m.mean$upper,
+  n = m.mean$n)
+
+forest_plot_input
+forest_plot_input <- forest_plot_input %>% arrange(SMD) %>% 
+  dplyr::mutate(Index = seq(1:26))
+forest_plot_input
+
+
+forest_plot_input_total <- data.frame(
+  Index = (26+1), ## This provides an order to the data
+  label = "All tillage",
+  SMD = m.mean$TE.random,
+  LL = m.mean$lower.random,
+  UL =  m.mean$upper.random,
+  n =  sum(m.mean$n))
+forest_plot_input_total
+
+forest_plot_input <- rbind(forest_plot_input,forest_plot_input_total)
+forest_plot_input
+
+
+meta::forest(m.mean, 
+             sortvar = mean,
+             prediction = TRUE, 
+             print.tau2 = FALSE,
+             leftlabs = c("Author", "g", "SE"))
+
+meta::forest(m.mean, layout = "JAMA")
+
+### comments - I need to modify data set so that the controls are better represented here.
+### I think this is ok because I am using the natural log .
+### Pull out new total from the results
+
+
+
+forest_plot_input <- forest_plot_input %>% 
+  mutate(label2 = paste0(label, "(", n, ")"))
+forest_plot_input
+forest_plot_input$label2
+str(forest_plot_input)
+
+
+forest_plot_input <- forest_plot_input %>%
+  mutate(label2 = fct_reorder(label2, Index)) %>%
+  arrange(label2)
+
+
+plot1 <- ggplot(forest_plot_input, aes(y = label2, x = SMD)) +
+  geom_point(shape = 18, size = 5) +  
+  geom_errorbarh(aes(xmin = LL, xmax = UL), height = 0.25) +
+  geom_vline(xintercept = 0, color = "red", linetype = "dashed", cex = 1, alpha = 0.5) +
+  xlab("Relative yield change (95% CI)") + 
+  ylab(" ") + 
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.text.y = element_text(size = 12, colour = "black"),
+        axis.text.x.bottom = element_text(size = 12, colour = "black"),
+        axis.title.x = element_text(size = 12, colour = "black"))
+plot1
+summary(m.mean)
+################################################################################
+# forest plot with means and stdev rather than output from meta analysis
+str(df_modified)
+
+
+
+df_modified_summary_site_tillage <- df_modified %>% dplyr::group_by(tillage_class, site_display) %>% 
+  dplyr::summarise(mean = mean( relative_yld_change, na.rm = TRUE),
+            sd = sd( relative_yld_change, na.rm = TRUE),
+            n = n(),
+            SE = sd/sqrt(n) )
+df_modified_summary_site_tillage <- ungroup(df_modified_summary_site_tillage)
+dim(df_modified_summary_site_tillage)
+
+df_modified_summary_site_tillage <- df_modified_summary_site_tillage %>% arrange(mean) %>% 
+  dplyr::mutate(Index = seq(1:42))
+df_modified_summary_site_tillage
+
+#### all data
+
+df_modified_summary_all <- df_modified %>% dplyr::group_by() %>% 
+  dplyr::summarise(mean = mean( relative_yld_change, na.rm = TRUE),
+            sd = sd( relative_yld_change, na.rm = TRUE),
+            n = n(),
+            SE = sd/sqrt(n) )
+df_modified_summary_all <- ungroup(df_modified_summary_all)
+df_modified_summary_all
+df_modified_summary_all <- df_modified_summary_all %>% dplyr::arrange() %>% 
+  dplyr::mutate(Index = 47)
+
+df_modified_summary_tillage <- df_modified %>% dplyr::group_by(tillage_class) %>% 
+  dplyr::summarise(mean = mean( relative_yld_change, na.rm = TRUE),
+            sd = sd( relative_yld_change, na.rm = TRUE),
+            n = n(),
+            SE = sd/sqrt(n) )
+df_modified_summary_tillage <- ungroup(df_modified_summary_tillage)
+df_modified_summary_tillage
+df_modified_summary_tillage <- df_modified_summary_tillage %>% dplyr::arrange() %>% 
+ dplyr::mutate(Index = c( (42+1),(42+2), (42+3), +(42+4) ))
+
+df_modified_summary_all <- df_modified_summary_all %>% 
+  dplyr::mutate(tillage_class = "All tillage",
+         site_display = "All sites")
+df_modified_summary_all
+df_modified_summary_tillage <- df_modified_summary_tillage %>% 
+  dplyr::mutate(site_display = tillage_class)
+
+df_modified_summary_tillage
+
+df_modified_summary_site_tillage
+
+
+df_modified_summary_v2 <- rbind(df_modified_summary_all,df_modified_summary_tillage, df_modified_summary_site_tillage )
+
+df_modified_summary_v2 <- df_modified_summary_v2 %>% 
+  dplyr::mutate(label2 = paste0(site_display, "(", n, ")"))
+df_modified_summary_v2
+
+
+
+df_modified_summary_v2 <- df_modified_summary_v2 %>%
+  mutate(label2 = fct_reorder(label2, Index)) %>%
+  #mutate(label2 = fct_reorder(label2)) %>%
+  arrange(label2)
+
+tail(df_modified_summary_v2)
+
+
+plot2_mix <- df_modified_summary_v2 %>% 
+  filter(tillage_class  == "Mixing" | tillage_class  == "All tillage" ) %>% 
+  ggplot( aes(y = label2, x = mean)) +
+  geom_point(shape = 18, size = 5) +  
+  geom_errorbarh(aes(xmin = mean-SE, xmax = mean+SE), height = 0.25) +
+  geom_vline(xintercept = 0, color = "red", linetype = "dashed", cex = 1, alpha = 0.5) +
+  xlab("Relative yield change (SE)") + 
+  ylab(" ") + 
+  xlim(-20, 150)+
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.text.y = element_text(size = 12, colour = "black"),
+        axis.text.x.bottom = element_text(size = 12, colour = "black"),
+        axis.title.x = element_text(size = 12, colour = "black") ) 
+        #axis.title.x=element_blank(),
+        #axis.text.x=element_blank(),
+        #axis.ticks.x=element_blank())
+plot2_mix
+
+plot2_rip <- df_modified_summary_v2 %>% 
+  filter(tillage_class  == "Ripping" | tillage_class  == "All tillage" ) %>% 
+  ggplot( aes(y = label2, x = mean)) +
+  geom_point(shape = 18, size = 5) +  
+  geom_errorbarh(aes(xmin = mean-SE, xmax = mean+SE), height = 0.25) +
+  geom_vline(xintercept = 0, color = "red", linetype = "dashed", cex = 1, alpha = 0.5) +
+  xlab("Relative yield change (SE)") + 
+  ylab(" ") +
+  xlim(-20, 150)+
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.text.y = element_text(size = 12, colour = "black"),
+        axis.text.x.bottom = element_text(size = 12, colour = "black"),
+        axis.title.x = element_text(size = 12, colour = "black") ) 
+#axis.title.x=element_blank(),
+#axis.text.x=element_blank(),
+#axis.ticks.x=element_blank())
+plot2_rip
+
+plot2_comb <- df_modified_summary_v2 %>% 
+  filter(tillage_class  == "Combination" | tillage_class  == "All tillage" ) %>% 
+  ggplot( aes(y = label2, x = mean)) +
+  geom_point(shape = 18, size = 5) +  
+  geom_errorbarh(aes(xmin = mean-SE, xmax = mean+SE), height = 0.25) +
+  geom_vline(xintercept = 0, color = "red", linetype = "dashed", cex = 1, alpha = 0.5) +
+  xlab("Relative yield change (SE)") + 
+  ylab(" ") + 
+  xlim(-20, 150)+
+  theme_bw() +
+  theme(panel.border = element_blank(),
+        panel.background = element_blank(),
+        panel.grid.major = element_blank(), 
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        axis.text.y = element_text(size = 12, colour = "black"),
+        axis.text.x.bottom = element_text(size = 12, colour = "black"),
+        axis.title.x = element_text(size = 12, colour = "black") ) 
+#axis.title.x=element_blank(),
+#axis.text.x=element_blank(),
+#axis.ticks.x=element_blank())
+plot2_comb
---
+++ All_sites_cleaned/Plots for SS conf Plot 1 constraint at sites description.R
@@ -0,0 +1,166 @@
+library(ggplot2)
+library(readxl)
+library(tidyverse)
+library(stringr)
+
+## plot and analsyis for preso constraints at sandy soil sites
+
+df <- read.csv("N:/sandy soils conference/data/All_sites_cleaned/control_metadata_contraints_tillage_only_cleaned.csv" )
+
+
+
+
+################################################################################
+## constraints at sandy soils sites
+str(df)
+ss_constaints <- df %>% distinct(site_display, .keep_all = TRUE) %>% 
+  select(site_display, Repellence, Acidity, Physical, Nutrient)  
+  
+str(ss_constaints)
+print(ss_constaints)
+#### 
+
+ss_constaints_summary_R <- ss_constaints %>%  group_by() %>% 
+  summarise(No_problem = (length(Repellence[Repellence==0])/26),
+            Moderate =   (length(Repellence[Repellence==1])/26),
+            Serve =      (length(Repellence[Repellence==2])/26)) %>% 
+  mutate(Constraint = "Repellence")
+ss_constaints_summary_R
+
+ss_constaints_summary_A <- ss_constaints %>%  group_by() %>% 
+  summarise(No_problem = (length(Acidity[Acidity==0])/26),
+            Moderate =   (length(Acidity[Acidity==1])/26),
+            Serve =      (length(Acidity[Acidity==2])/26)) %>% 
+  mutate(Constraint = "Acidity")
+
+ss_constaints_summary_P <- ss_constaints %>%  group_by() %>% 
+  summarise(            
+    No_problem = (length(Physical[Physical==0])/26),
+    Moderate =   (length(Physical[Physical==1])/26),
+    Serve =      (length(Physical[Physical==2])/26))%>% 
+  mutate(Constraint = "Physical")
+ss_constaints_summary_P
+
+ss_constaints_summary_N <- ss_constaints %>%  group_by() %>% 
+  summarise( No_problem = (length(Nutrient[Nutrient==0])/26),
+             Moderate =   (length(Nutrient[Nutrient==1])/26),
+             Serve =      (length(Nutrient[Nutrient==2])/26))%>% 
+  mutate(Constraint = "Nutrient")
+
+ss_constaints_summary <- rbind(ss_constaints_summary_R, 
+                               ss_constaints_summary_A,
+                               ss_constaints_summary_P,
+                               ss_constaints_summary_N)
+
+ss_constaints_summary
+
+ss_constaints_summary_long <- ss_constaints_summary %>% 
+  pivot_longer(cols = c(No_problem:Serve),
+               names_to = "rank",
+               values_to = "percent")
+
+ss_constaints_summary_long
+
+
+ss_constaints_multiple <-  ss_constaints %>% 
+  mutate(multiple_mod_severe_R = case_when(Repellence == 1 |Repellence == 2 ~ 1, .default = 0)) %>% 
+  mutate(multiple_mod_severe_A = case_when(Acidity == 1 |Acidity == 2 ~ 1, .default = 0)) %>% 
+  mutate(multiple_mod_severe_N = case_when(Nutrient == 1 |Nutrient == 2 ~ 1, .default = 0)) %>% 
+  mutate(multiple_mod_severe_P = case_when(Physical == 1 |Physical == 2 ~ 1, .default = 0)) %>% 
+         
+  mutate(multiple_mod_severe = (
+           multiple_mod_severe_R+ 
+           multiple_mod_severe_A+ 
+           multiple_mod_severe_P+
+           multiple_mod_severe_N)) %>% 
+  select(site_display,multiple_mod_severe )
+
+
+ss_constaints_summary_Numb <- ss_constaints_multiple %>%  group_by() %>% 
+  summarise(constraints2 = (length(multiple_mod_severe[multiple_mod_severe==2])/26)*100,
+            constraints3 = (length(multiple_mod_severe[multiple_mod_severe==3])/26)*100,
+            constraints4 = (length(multiple_mod_severe[multiple_mod_severe==4])/26)*100)
+ss_constaints_summary_Numb       
+
+
+            
+ss_constaints_summary_long
+## order 
+ss_constaints_summary_long$rank <- factor(ss_constaints_summary_long$rank, 
+                                                       levels = c(
+                                                         "No_problem" ,
+                                                         "Moderate",
+                                                         "Serve"
+                                                       ),
+                                                       labels = c(
+                                                         "No problem",
+                                                         "Moderate probelm",
+                                                         "Serve probelm"
+                                                       ))
+
+ss_constaints_summary_long$Constraint <- factor(ss_constaints_summary_long$Constraint, 
+                                          levels = c(
+                                            "Acidity" ,
+                                            "Repellence",
+                                            "Nutrient",
+                                            "Physical"))
+
+ss_constaints_summary_long
+
+### Plot
+plot_constraints <- ss_constaints_summary_long %>%
+  ggplot(aes(y = percent, x = Constraint , fill = rank)) +
+  geom_col() +
+  scale_fill_manual(values=c("lightgrey", "cornflowerblue", "darkblue"))+
+  scale_y_continuous(labels = scales::percent)+
+  theme_classic() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        #legend.position="bottom",
+        legend.title = element_blank())+
+  
+  xlab("") + 
+  ylab("Precentage of site") + 
+  labs(title = "")  
+plot_constraints
+
+
+################################################################################
+
+## number of sites
+number_of_sites <- df %>% select(site_display, site) %>% distinct(site, .keep_all = TRUE)
+number_of_sites
+dim(number_of_sites)
+
+## Years trials were run
+max(df$year)
+min(df$year)
+max(df$year) - min(df$year)
+
+## number of different treatments
+df %>%  dplyr::distinct(Descriptors) %>% count()
+str(df)
+treatments_with_tillage <- df %>%  group_by(tillage_amendments_class, Descriptors) %>% 
+  summarise(n = n())
+length(treatments_with_tillage$n)
+
+treatments_with_tillage %>%  group_by(tillage_amendments_class) %>% 
+  summarise(n = n())
+
+str(df)
+df %>%  group_by( ) %>% 
+  summarise(n = n())
+
+tillage_percent <- df %>%  group_by(tillage_class) %>% 
+  summarise(n = n())
+tillage_percent <- tillage_percent %>% 
+  dplyr::mutate(percentage_tillage_type = (n /2458)*100) %>% 
+  dplyr::mutate(percentage_tillage_type_round = round(percentage_tillage_type,0))
+tillage_percent
+
+
+tillage_amen_class_percent <- df %>%  group_by(tillage_amendments_class) %>% 
+  summarise(n = n())
+tillage_amen_class_percent <- tillage_amen_class_percent %>% 
+  dplyr::mutate(percentage_tillage_type = (n /2458)*100) %>% 
+  dplyr::mutate(percentage_tillage_type_round = round(percentage_tillage_type,0))
+tillage_amen_class_percent
